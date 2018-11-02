b141879b01b01a9aa33e399d9c8e93a34b233fef
adnan29sk <44690562+adnan29sk@users.noreply.github.com>
2018-11-02 08:54:31 +0530

    uploaded source code

---
+++ dd2.py
@@ -0,0 +1,95 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:54:40) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/random/AppData/Local/Programs/Python/Python36/dd.py ===
+Wecome to GCI 2018
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+>>> 
+=== RESTART: C:/Users/random/AppData/Local/Programs/Python/Python36/dd.py ===
+Wecome to GCI 2018
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+Hello! Enteryour name: Adnan
+Hello Adnan  please to meet you! Did you know that your name backwards is solraC?
+>>> 
+=== RESTART: C:/Users/random/AppData/Local/Programs/Python/Python36/dd.py ===
+Wecome to GCI 2018
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+Hello! Enteryour name: adnan
+Hello adnan  please to meet you! Did you know that your name backwards is adnan ?
+>>> 
+>>> 
+=== RESTART: C:/Users/random/AppData/Local/Programs/Python/Python36/dd.py ===
+Wecome to GCI 2018
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+Hello! Enteryour name: amit
+Traceback (most recent call last):
+  File "C:/Users/random/AppData/Local/Programs/Python/Python36/dd.py", line 8, in <module>
+    str.reverse(name)
+AttributeError: type object 'str' has no attribute 'reverse'
+>>> 
+=== RESTART: C:/Users/random/AppData/Local/Programs/Python/Python36/dd.py ===
+Wecome to GCI 2018
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+Hello! Enteryour name: mait
+Hello mait  please to meet you! Did you know that your name backwards is tiam ?
+>>> 
+=== RESTART: C:/Users/random/AppData/Local/Programs/Python/Python36/dd.py ===
+Wecome to GCI 2018
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+GCI is Great
+Hello! Enteryour name: darsh
+Hello darsh  please to meet you! Did you know that your name backwards is hsrad ?
+>>> 
